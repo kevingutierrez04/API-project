32e38f1715c06378988ca8af2f5a73386f00639e
kmg2226 Kevin Gutierrez Garcia <kmg2226@columbia.edu>
2024-06-24 01:30:23 +0000

    added a database to store GPT answers. Not fully functional yet. Also added requirements.txt for dependancies when starting project on new IDE.

--- tbd.py
+++ tbd.py
@@ -2,6 +2,12 @@ import os
 from openai import OpenAI
 from dotenv import load_dotenv
 
+#new imports
+import requests
+import pandas as pd
+import sqlalchemy as db
+import json
+
 load_dotenv()
 
 #initialize client with API key from .env
@@ -12,9 +18,29 @@ client = OpenAI(
 #Starting a chat with GPT3.5
 stream = client.chat.completions.create(
     model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": "Say this is a test"}],
+    response_format={ "type": "json_object"},
+    messages=[
+      {"role": "system", "content": "You are a helpful assistant designed to output JSON. Surround the value of every JSON value with square brackets"}, 
+      {"role": "user", "content": '''
+    Give me a one paragraph explanation about the nintendo game Splatoon. 
+    '''}],
+    
     stream=True,
 )
+test = ""
 for chunk in stream:
-    if chunk.choices[0].delta.content is not None:
-        print(chunk.choices[0].delta.content, end="")
+  if chunk.choices[0].delta.content:
+    test += chunk.choices[0].delta.content
+ans = json.loads(test)
+
+for key in ans:
+  if isinstance(ans[key], list):
+    ans[key] = '. '.join([str(item) for item in ans[key]])
+
+epic = pd.DataFrame.from_dict([ans])
+engine = db.create_engine('sqlite:///placeholder.db')
+epic.to_sql('Responses', con=engine, if_exists='replace', index=False)
+with engine.connect() as connection:
+  query_result = connection.execute(db.text("SELECT * FROM Responses;")).fetchall()
+  print(pd.DataFrame(query_result))
+  print(chunk.choices[0].delta.content, end="")
